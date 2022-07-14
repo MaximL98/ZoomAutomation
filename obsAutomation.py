4a75f46fe50f9013bbfc55c2991db840fb57daaa
Maxim student <lebedinsky.maxim@gmail.com>
2022-07-14 12:34:31 +0300

    controling OBS now not with hotkeys, but with OBS-websocket

--- obsAutomation.py
+++ obsAutomation.py
@@ -1,58 +1,60 @@
+import logging
 import os
+import sys
 import time
-import pygetwindow as gw
-from pynput.keyboard import Controller
 import psutil
+
+from obswebsocket import obsws, requests
+from settings import HOST, PORT, PASSWORD, OBS_PATH
 
 
 def open_obs():
+    # checks if OBS not already running if not opens it
     if not "obs64.exe" in (p.name() for p in psutil.process_iter()):
         # opens OBS following the given path
-        application = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OBS Studio\OBS Studio (64bit).lnk"
+        application = OBS_PATH
         os.startfile(application)
         time.sleep(5)
 
 
+def close_obs():
+    # checks if OBS is running, if yes, closes it
+    if "obs64.exe" in (p.name() for p in psutil.process_iter()):
+        # close OBS
+        os.system("TASKKILL /F /IM obs64.exe")
+
+
+def obs_connection():
+    # opens OBS
+    open_obs()
+    time.sleep(1)
+    # logs into OBS-WEBSOCKET
+    logging.basicConfig(level=logging.INFO)
+    sys.path.append('../')
+    # host name
+    host = HOST
+    # port name given by user
+    port = PORT
+    # password given by user
+    password = PASSWORD
+    ws = obsws(host, port, password)
+    return ws
+
+
 # starts obs and making it the active window
-def obs_start():
-    # activating OBS
-    win = gw.getWindowsWithTitle('OBS')[0]
-    win.activate()
-    time.sleep(10)
-    # calling the hotkeys
-    hotkey_e()
+def obs_start(ws):
+    # connects to OBS websocket
+    ws.connect()
+    # starts recording
+    ws.call(requests.StartRecording())
+    print("OBS started recording")
 
 
-# TODO stop recording
-def obs_end():
-    win = gw.getWindowsWithTitle('OBS')[0]
-    win.activate()
-    time.sleep(2)
-    hotkey_end()
-
-
-# hotkeys
-def hotkey_e():
-    # calling the controller
-    keyboard = Controller()
-    # switch scenes hotkey
-    switch_to_scene = "a"
-    # start recording hotkey
-    start_recording = "r"
-    # presses and releases
-    keyboard.press(switch_to_scene)
-    time.sleep(0.05)
-    keyboard.release(switch_to_scene)
-    time.sleep(0.05)
-    keyboard.press(start_recording)
-    time.sleep(0.05)
-    keyboard.release(start_recording)
-
-
-def hotkey_end():
-    keyboard = Controller()
-    end_recording = "s"
-    keyboard.press(end_recording)
-    time.sleep(0.05)
-    keyboard.release(end_recording)
-# HOTKEY = [HotKey(HotKey.parse("a"), hotkey_e())]
+def obs_end(ws):
+    # stops recording
+    ws.call(requests.StopRecording())
+    print("OBS stopped recording")
+    # disconnecting from OBS websocket
+    ws.disconnect()
+    # closes OBS
+    close_obs()
