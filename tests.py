2bcae60f208d778840fc935e6c7136b6ab571de7
Maxim student <lebedinsky.maxim@gmail.com>
2022-07-25 07:48:07 +0300

    added comments

--- tests.py
+++ tests.py
@@ -2,9 +2,12 @@ import urllib.request
 
 
 def have_internet():
+    # trying to open url
     try:
         urllib.request.urlopen('http://216.58.192.142', timeout=2)
+        # if successful, return True meaning there is internet connection
         return True
     except:
+        # else, there is no internet connection and False is returned
         return False
 
